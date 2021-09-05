634738b2319312640c569612830938c2da37e343
Slavek592 <71459428+Slavek592@users.noreply.github.com>
2021-09-05 13:15:39 +0200

    Error fixing

--- NiLeSh/GraphicalNiLeSh.sh
+++ NiLeSh/GraphicalNiLeSh.sh
@@ -4,5 +4,5 @@ cd "$scriptdir"
 cd ../
 echo This version uses tkinter for GUI. If You have not installed it, try running this command:
 echo sudo apt-get install python3-tk
-python3 NiLeSh/InteractiveNiLeSh.py
+python3 NiLeSh/GraphicalNiLeSh.py
 

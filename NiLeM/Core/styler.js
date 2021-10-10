077113f33b4022728b78f5176ea983c66f6576ab
Slavek592 <viacheslav.nikiforov@mensagymnazium.cz>
2021-10-10 10:04:30 +0200

    Change main files - new colors

--- NiLeM/Core/styler.js
+++ NiLeM/Core/styler.js
@@ -10,7 +10,8 @@ function Set()
         var style = document.createElement("style");
         if (window.localStorage.getItem("color") == "dark")
         {
-            style.innerHTML = "body {"
+            style.innerHTML = ""
+                + "body {"
                 + "background-color: black;"
                 + "color: white;"
                 + "}"
@@ -27,7 +28,8 @@ function Set()
         }
         else if (window.localStorage.getItem("color") == "light")
         {
-            style.innerHTML = "body {"
+            style.innerHTML = ""
+                + "body {"
                 + "background-color: white;"
                 + "color: black;"
                 + "}"
@@ -40,6 +42,60 @@ function Set()
                 + "    background-color: #F0F0F0;"
                 + "    color: black;"
                 + "    border-color: gray;"
+                + "}";
+        }
+        else if (window.localStorage.getItem("color") == "fire")
+        {
+            style.innerHTML = ""
+                + "body {"
+                + "background-color: red;"
+                + "color: black;"
+                + "}"
+                + "button {"
+                + "    background-color: #F02F2F;"
+                + "    color: black;"
+                + "    border-color: #7F0000;"
+                + "}"
+                + "input {"
+                + "    background-color: #F02F2F;"
+                + "    color: black;"
+                + "    border-color: #7F0000;"
+                + "}";
+        }
+        else if (window.localStorage.getItem("color") == "water")
+        {
+            style.innerHTML = ""
+                + "body {"
+                + "background-color: blue;"
+                + "color: black;"
+                + "}"
+                + "button {"
+                + "    background-color: #2F2FF0;"
+                + "    color: black;"
+                + "    border-color: #00007F;"
+                + "}"
+                + "input {"
+                + "    background-color: #2F2FF0;"
+                + "    color: black;"
+                + "    border-color: #00007F;"
+                + "}";
+        }
+        else if (window.localStorage.getItem("color") == "grass")
+        {
+            style.innerHTML = ""
+                + "body {"
+                + "background-color: green;"
+                + "color: black;"
+                + "}"
+                + "button {"
+                + "    background-color: #2FF02F;"
+                + "    color: black;"
+                + "    border-color: #007F00;"
+                + "}"
+                + "input {"
+                + "    background-color: #2FF02F;"
+                + "    color: black;"
+                + "    border-color: #007F00;"
                 + "}";
         }
         document.head.appendChild(style);

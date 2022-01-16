0a5360c317b4d728e441bdb7f66d2b82b94981ad
Slavek592 <viacheslav.nikiforov@mensagymnazium.cz>
2022-01-16 16:11:11 +0100

    Put http requests into try

--- NiLeM/Updating.py
+++ NiLeM/Updating.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-import requests
+try:
+    import requests
+except:
+    print("Unable update databases. Try installing requests.")
 
 
 def update(language):
-    response = requests.get(
-        "https://raw.github.com/Slavek592/NiLeMProject/desktop/Data/"
-        + language.capitalize() + ".db", verify=False, headers={})
-    if response.status_code == 200:
-        file = open("Data/" + language.capitalize() + ".db", "wb")
-        file.write(response.content)
+    try:
+        response = requests.get(
+            "https://raw.github.com/Slavek592/NiLeMProject/desktop/Data/"
+            + language.capitalize() + ".db", verify=False, headers={})
+        if response.status_code == 200:
+            file = open("Data/" + language.capitalize() + ".db", "wb")
+            file.write(response.content)
+    except:
+        print("I am sorry, it was not successful. Try checking internet connection.")
 
 
 def complete_update():
